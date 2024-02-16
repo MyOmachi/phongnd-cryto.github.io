6d82af175fa74d1613c5699d2c4df4d2077ba8a6
phongnd <nguyendinhphong.ft@gmail.com>
2024-02-16 14:34:48 +0700

    update add firestore to project

---
+++ src/app/views/tokens-list/modals/tokens.models.ts
@@ -0,0 +1,11 @@
+export interface IToken {
+    id: string;
+    name: string;
+    symbol: string;
+    tags: string[];
+    coingeckoUrl: string;
+    coinmarketcapUrl: string;
+    cex: string;
+    images: string;
+    note: string;
+}
--- src/app/views/tokens-list/tokens-list.component.ts
+++ src/app/views/tokens-list/tokens-list.component.ts
@@ -11,4 +11,7 @@ import { Firestore } from '@angular/fire/firestore';
 })
 export class TokensListComponent {
   firestore: Firestore = inject(Firestore);
+  
+  constructor() {
+  }
 }
